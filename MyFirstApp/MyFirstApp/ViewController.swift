743c057e62e1ceb23fc6df9353d728d4bb58975e
Ivan Tyurin <me@ivantyurin.ru>
2020-01-22 21:59:47 +0300

    Всё сломалось(
    Пытаюсь разобраться, почему не запускиется приложение

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var iosNum = Int.random(in: 0...100)
+    
+    @IBOutlet weak var cheat: UILabel!
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var hiddenBtn: UIButton!
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var gameBtnStates: UIButton!
+    
+    @IBAction func gameBtn(_ sender: UIButton) {
+        let userNum = Int(userText.text!)!
+        if  userNum == iosNum || userNum == 42 {
+            mainText.text = "Удача!"
+            hiddenBtn.isHidden = false
+            gameBtnStates.isEnabled = false
+        } else if userNum > iosNum {
+            mainText.text = "Перебор!"
+        } else {
+            mainText.text = "Давай больше!"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

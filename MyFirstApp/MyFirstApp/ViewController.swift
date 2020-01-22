9eee0ea38a038539831bbe1e248e4a6992988061
Ivan Tyurin <me@ivantyurin.ru>
2020-01-22 21:33:42 +0300

    First save
    Just create the project

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Ivan Tyurin on 22.01.2020.
+//  Copyright © 2020 Ivan Tyurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

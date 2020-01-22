ef4f1be58f03d1fd2b73801d9d9e73d2fdda409c
Ivan Tyurin <me@ivantyurin.ru>
2020-01-22 22:58:31 +0300

    add old project files

--- MyFirstAppAdditional/MyFirstAppAdditional/ViewController.swift
+++ MyFirstAppAdditional/MyFirstAppAdditional/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MyFirstAppAdditional
+//  MyFirstApp
 //
 //  Created by Ivan Tyurin on 22.01.2020.
 //  Copyright © 2020 Ivan Tyurin. All rights reserved.
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var iosNum = Int.random(in: 0...100)
+    
+    @IBOutlet weak var cheat: UILabel!
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var hiddenBtn: UIButton!
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var gameBtnStates: UIButton!
+    
+    @IBAction func gameBtn(_ sender: UIButton) {
+        let userNum = Int(userText.text!)!
+        if  userNum == iosNum || userNum == 42 {
+            mainText.text = "Удача!"
+            hiddenBtn.isHidden = false
+            gameBtnStates.isEnabled = false
+        } else if userNum > iosNum {
+            mainText.text = "Перебор!"
+        } else {
+            mainText.text = "Давай больше!"
+        }
+    }
+    
+    @IBAction func newGameBtn(_ sender: UIButton) {
+        iosNum = Int.random(in: 0...100)
+        gameBtnStates.isEnabled = true
+        hiddenBtn.isHidden = true
+        mainText.text = "Давай ещё разок?"
+        cheat.text = String(iosNum)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainText.text = "Испытай свою удачу..."
+        cheat.text = String(iosNum)
     }
-
-
 }
 

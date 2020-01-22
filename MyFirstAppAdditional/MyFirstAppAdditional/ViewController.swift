09a5d4b3c523f079224546ddd2f4b89a03fae1cf
Ivan Tyurin <me@ivantyurin.ru>
2020-01-23 00:26:10 +0300

    One part
    Did the simplest task with a star

--- MyFirstAppAdditional/MyFirstAppAdditional/ViewController.swift
+++ MyFirstAppAdditional/MyFirstAppAdditional/ViewController.swift
@@ -11,15 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     var iosNum = Int.random(in: 0...100)
+    var tryNumbers: Int = 0
     
     @IBOutlet weak var cheat: UILabel!
     @IBOutlet weak var mainText: UILabel!
     @IBOutlet weak var hiddenBtn: UIButton!
     @IBOutlet weak var userText: UITextField!
     @IBOutlet weak var gameBtnStates: UIButton!
+    @IBOutlet weak var gameCounter: UILabel!
     
     @IBAction func gameBtn(_ sender: UIButton) {
         let userNum = Int(userText.text!)!
+        tryNumbers += 1
+        gameCounter.text = "Количество попыток: " + String(tryNumbers)
         if  userNum == iosNum || userNum == 42 {
             mainText.text = "Удача!"
             hiddenBtn.isHidden = false
@@ -37,6 +41,8 @@ class ViewController: UIViewController {
         hiddenBtn.isHidden = true
         mainText.text = "Давай ещё разок?"
         cheat.text = String(iosNum)
+        tryNumbers = 0
+        gameCounter.text = "Количество попыток: " + String(tryNumbers)
     }
     
     
@@ -45,6 +51,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         mainText.text = "Испытай свою удачу..."
         cheat.text = String(iosNum)
+        tryNumbers = 0
+        gameCounter.text = "Количество попыток: " + String(tryNumbers)
     }
 }
 

ade340491891c44981258d470259993ecf999c4e
Ivan Tyurin <me@ivantyurin.ru>
2020-03-03 14:35:32 +0300

    Create Project

---
+++ StarWars/StarWars/StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Ivan Tyurin on 03.03.2020.
+//  Copyright © 2020 Ivan Tyurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
